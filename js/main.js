c42998a7658e36f4b381e38be6b890f4e1ec4834
Alex Dzyuba <alexeydzyuba27@gmail.com>
2024-08-20 15:14:13 +0300

    w

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
-console.log(123)
+// SLIDER
+const track = document.querySelector('.slider-track')
+const slideWidth = document.querySelector('.slider-track video').clientWidth
+const timeLine = document.querySelector('.time-line')
+const TIME = 1000
+function slide(mult) {
+  track.style.transform = `translate3d(-${slideWidth * mult}px, 0, 0)`
+}
 
+// setInterval(() => {
+//   slide(1)
+
+//   setInterval(() => {
+//     slide(2)
+
+//     setInterval(() => {
+//       slide(3)
+//     }, 2000)
+//   }, 2000)
+// }, 2000)
+
+// const timeLineInterval = setInterval(() => {
+//   timeLine.style.width -= 1
+// }, 10)
+
+// timeLine.style.width = '700px'
