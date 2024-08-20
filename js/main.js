812e4e2ddeea1584b2aefe2d76c1118db3fc9e71
Alex Dzyuba <alexeydzyuba27@gmail.com>
2024-08-20 16:31:24 +0300

    w

--- js/main.js
+++ js/main.js
@@ -2,25 +2,44 @@
 const track = document.querySelector('.slider-track')
 const slideWidth = document.querySelector('.slider-track video').clientWidth
 const timeLine = document.querySelector('.time-line')
-const TIME = 1000
+const dots = document.querySelector('.dots').children
+let activeSlideIdx = 0
+const TIME = 5000 // 8000
+
+timeLine.style.transition = `width ${TIME / 1000 + 's'} linear`
+timeLine.classList.add('time-line--disappear')
+let sliderInterval = setInterval(() => {
+  timeLine.style.width = ''
+  timeLine.classList.add('time-line--disappear')
+
+  activeSlideIdx++
+  if (activeSlideIdx > 2) activeSlideIdx = 0
+
+  dots[activeSlideIdx].click()
+
+  timeLine.style.width = '100%'
+}, 5000)
+
+for (let dot of dots) {
+  dot.addEventListener('mouseover', () => {
+    if (dot.classList.contains('dot--active')) return
+    dot.classList.add('dot--hovered')
+  })
+  dot.addEventListener('mouseout', () => {
+    dot.classList.remove('dot--hovered')
+  })
+
+  dot.addEventListener('click', () => {
+    activeSlideIdx = Number(dot.dataset.idx)
+
+    slide(activeSlideIdx)
+
+    document.querySelector('.dot--active').classList.remove('dot--active')
+    dot.classList.add('dot--active')
+    dot.classList.remove('dot--hovered')
+  })
+}
+
 function slide(mult) {
   track.style.transform = `translate3d(-${slideWidth * mult}px, 0, 0)`
 }
-
-// setInterval(() => {
-//   slide(1)
-
-//   setInterval(() => {
-//     slide(2)
-
-//     setInterval(() => {
-//       slide(3)
-//     }, 2000)
-//   }, 2000)
-// }, 2000)
-
-// const timeLineInterval = setInterval(() => {
-//   timeLine.style.width -= 1
-// }, 10)
-
-// timeLine.style.width = '700px'
